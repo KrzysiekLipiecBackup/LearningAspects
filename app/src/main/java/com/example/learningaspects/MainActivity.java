3bc7c62668b601a53e29e269fb5bb7ed57a7cc4d
ThePivko <krzysiekl57991@gmail.com>
2020-05-10 23:20:24 +0200

    Strona tytułowa ver.1, dodana navigacja na dole ekranu.

--- app/src/main/java/com/example/learningaspects/MainActivity.java
+++ app/src/main/java/com/example/learningaspects/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.learningaspects;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "com.example.learningaspects.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void button1Pressed(View view) {
+        Intent intent = new Intent(this, MainActivity_2.class);
+        startActivity(intent);
+    }
+
 }
