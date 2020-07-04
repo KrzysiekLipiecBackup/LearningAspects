c4b474da7e666b5b0153febd9520be8f981864a8
ThePivko <krzysiekl57991@gmail.com>
2020-07-04 22:38:01 +0200

    Dodana lista czasowników w Dashboard

--- app/src/main/java/com/example/learningaspects/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/learningaspects/ui/dashboard/DashboardFragment.java
@@ -11,10 +11,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+import com.example.learningaspects.Czasownik;
+import com.example.learningaspects.CzasownikAdapter;
 import com.example.learningaspects.R;
 
 public class DashboardFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;  /* pozwala na załadowanie tylko tyle co trzeba a nie wszystkich*/
+    private RecyclerView.LayoutManager mLayoutManager;
 
     private DashboardViewModel dashboardViewModel;
 
@@ -23,13 +32,38 @@ public class DashboardFragment extends Fragment {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        ArrayList<Czasownik> czasownikList = new ArrayList<>();
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "robić", "zrobić"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_black_24dp, "oglądać", "obejrzeć"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_black_24dp, "pić", "wypić"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "robić", "zrobić"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "oglądać", "obejrzeć"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "pić", "wypić"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "robić", "zrobić"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "oglądać", "obejrzeć"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "pić", "wypić"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "robić", "zrobić"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "oglądać", "obejrzeć"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "pić", "wypić"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "robić", "zrobić"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "oglądać", "obejrzeć"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "pić", "wypić"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "robić", "zrobić"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "oglądać", "obejrzeć"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "pić", "wypić"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "robić", "zrobić"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "oglądać", "obejrzeć"));
+        czasownikList.add(new Czasownik(R.drawable.ic_star_border_black_24dp, "pić", "wypić"));
+
+        mRecyclerView = root.findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mAdapter = new CzasownikAdapter(czasownikList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
         return root;
     }
 }
